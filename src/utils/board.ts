abb143dd1eb89aea0e999e3344e2f87bcef5c2ac
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-04-27 14:04:52 +0200

    Add createBoard function

--- src/utils/board.ts
+++ src/utils/board.ts
@@ -7,3 +7,22 @@ export type Position = {
     col: number;
 };
 
+export function createBoard(rows: number, cols: number): Board {
+    const numbers = Array.from({ length: rows * cols -1 }, (_, i) => i +1);
+
+    numbers.push(null);
+
+    const board: Board = [];
+    let index = 0;
+
+    for (let r = 0; r < rows; r++) {
+        const row: Tile[] = [];
+
+        for (let c = 0; c < cols; c++) {
+            row.push(numbers[index]);
+            index++;
+        }
+        board.push(row);
+    }
+    return board;
+}
