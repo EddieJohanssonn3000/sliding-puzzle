3aa04ccf6aa6742025b7480258d1e4dc5059baf7
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-04-27 14:28:33 +0200

    Implemented initial board in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-
+import { useState } from "react";
+import { createBoard } from "./utils/board";
+import type { Board } from "./utils/board";
 
 export default function App() {
-  return <h1>Sliding Puzzle</h1>;
+  const [board] = useState<Board>(createBoard(3, 4));
+
+  return (
+    <div>
+      <h1>Sliding Puzzle</h1>
+      {board.map((row, r) => (
+        <div key={r}>{row.map((tile, c) => ( <span key={c} style={{ margin: 10 }}>
+          {tile === null ? "_" : tile}
+        </span>
+        ))}
+        </div>
+      ))}
+    </div>
+  )
 }
