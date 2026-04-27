3be39eb690434588aacb433c09c0a13893bf06e1
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-04-27 14:49:18 +0200

    Created Board component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import { useState } from "react";
 import { createBoard } from "./utils/board";
-import type { Board } from "./utils/board";
+import type { Board as BoardType } from "./utils/board";
+import Board from "./components/Board";
 
 export default function App() {
-  const [board] = useState<Board>(createBoard(3, 4));
+  const [board] = useState<BoardType>(createBoard(3, 4));
 
   return (
     <div>
       <h1>Sliding Puzzle</h1>
-      {board.map((row, r) => (
-        <div key={r}>{row.map((tile, c) => ( <span key={c} style={{ margin: 10 }}>
-          {tile === null ? "_" : tile}
-        </span>
-        ))}
-        </div>
-      ))}
+      <Board board={board} />
     </div>
   )
 }
