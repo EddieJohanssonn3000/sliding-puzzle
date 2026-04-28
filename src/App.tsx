a8ad8c7450a673a7cb99085a99c8627cdfbd44ca
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-04-28 10:41:07 +0200

    Handle tile click in App with coordinates

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,15 @@ import Board from "./components/Board";
 
 export default function App() {
   const [board] = useState<BoardType>(createBoard(3, 4));
+  
+  function handleTileClick(row: number, col: number) {
+    console.log("clicked", row, col);
+  }
 
   return (
     <div>
       <h1>Sliding Puzzle</h1>
-      <Board board={board} />
+      <Board board={board} onTileClick={handleTileClick} />
     </div>
   )
 }
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,14 +3,15 @@ import Tile from "./Tile";
 
 type Props = {
     board: BoardType;
+    onTileClick: (row: number, col: number) => void;
 };
 
-export default function Board({ board }: Props) {
+export default function Board({ board, onTileClick }: Props) {
     return (
         <div>
             {board.map((row, r) => (
                 <div key={r}>
-                    {row.map((tile, c) => (<Tile key={c} value={tile} />))}
+                    {row.map((tile, c) => (<Tile key={c} value={tile} onClick={() => onTileClick(r, c)} />))}
                 </div>
             ))}
         </div>
